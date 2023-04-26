676d9a48833fd2bc1f766f7e52afc9fc0ff30e1b
离披 <lipi@shuise.dev>
2023-04-26 12:46:32 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarRailUnlock120
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var node = Registry.CurrentUser.OpenSubKey("Software")?.OpenSubKey("miHoYo")?.OpenSubKey("崩坏：星穹铁道");
+            if (node == null)
+            {
+                Console.WriteLine("打开注册表失败，请尝试至少运行一次游戏");
+                Console.ReadLine();
+                return;
+            }
+            var keyName = node.GetValueNames().FirstOrDefault(x => x.StartsWith("MIHOYOSDK_CONFIG_MODEL_NAME"));
+            if (keyName == default)
+            {
+                Console.WriteLine("获取注册表内容失败，请尝试至少运行一次游戏");
+                Console.ReadLine();
+                return;
+            }
+            var key = node.GetValue(keyName);
+            if (key == null)
+            {
+                Console.WriteLine("获取注册表内容失败，请尝试至少运行一次游戏");
+                Console.ReadLine();
+                return;
+            }
+            var value = Encoding.UTF8.GetString((byte[])key);
+            var json = JObject.Parse(value);
+            json["FPS"] = 120;
+            node.SetValue(keyName, Encoding.UTF8.GetBytes(json.ToString(Newtonsoft.Json.Formatting.None)));
+            Console.WriteLine("设置完成");
+            Console.ReadLine();
+        }
+    }
+}
