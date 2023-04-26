f897c4739756f3cbc5eaa164092ac90cf3b29aff
离披 <lipi@shuise.dev>
2023-04-26 15:05:35 +0800

    添加国际服&&fix

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,21 +16,21 @@ namespace StarRailUnlock120
             var node = Registry.CurrentUser.OpenSubKey("Software")?.OpenSubKey("miHoYo")?.OpenSubKey("崩坏：星穹铁道", true);
             if (node == null)
             {
-                Console.WriteLine("打开注册表失败，请尝试至少运行一次游戏");
+                Console.WriteLine("打开国服注册表失败，请尝试至少在游戏中修改一次图形设置并关闭设置界面");
                 Console.ReadLine();
                 return;
             }
-            var keyName = node.GetValueNames().FirstOrDefault(x => x.StartsWith("MIHOYOSDK_CONFIG_MODEL_NAME"));
+            var keyName = node.GetValueNames().FirstOrDefault(x => x.StartsWith("GraphicsSettings_Model"));
             if (keyName == default)
             {
-                Console.WriteLine("获取注册表内容失败，请尝试至少运行一次游戏");
+                Console.WriteLine("获取国服注册表内容失败，请尝试至少在游戏中修改一次图形设置并关闭设置界面");
                 Console.ReadLine();
                 return;
             }
             var key = node.GetValue(keyName);
             if (key == null)
             {
-                Console.WriteLine("获取注册表内容失败，请尝试至少运行一次游戏");
+                Console.WriteLine("获取国服注册表内容失败，请尝试至少在游戏中修改一次图形设置并关闭设置界面");
                 Console.ReadLine();
                 return;
             }
@@ -37,7 +38,32 @@ namespace StarRailUnlock120
             var json = JObject.Parse(value);
             json["FPS"] = 120;
             node.SetValue(keyName, Encoding.UTF8.GetBytes(json.ToString(Newtonsoft.Json.Formatting.None)));
-            Console.WriteLine("设置完成");
+            Console.WriteLine("国服设置完成");
+
+            var intlNode = Registry.CurrentUser.OpenSubKey("Software")?.OpenSubKey("Cognosphere")?.OpenSubKey("Star Rail", true);
+            if (intlNode != null)
+            {
+                var intlKeyName = intlNode.GetValueNames().FirstOrDefault(x => x.StartsWith("GraphicsSettings_Model"));
+                if (intlKeyName == null)
+                {
+                    Console.WriteLine("获取国际服注册表内容失败，请尝试至少在游戏中修改一次图形设置并关闭设置界面");
+                    Console.ReadLine();
+                    return;
+                }
+                var intlKey = intlNode.GetValue(intlKeyName);
+                if (intlKey == null)
+                {
+                    Console.WriteLine("获取国际服注册表内容失败，请尝试至少在游戏中修改一次图形设置并关闭设置界面");
+                    Console.ReadLine();
+                    return;
+                }
+                var intlValue = Encoding.UTF8.GetString((byte[])intlKey);
+                var intlJson = JObject.Parse(intlValue);
+                intlJson["FPS"] = 120;
+                intlNode.SetValue(intlKeyName, Encoding.UTF8.GetBytes(intlJson.ToString(Newtonsoft.Json.Formatting.None)));
+                Console.WriteLine("国际服设置完成");
+            }
+
             Console.ReadLine();
         }
     }
